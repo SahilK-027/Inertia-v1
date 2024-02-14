7b6ea082236f1624e5e9ee4c8961ce4a6243254c
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2024-02-14 12:43:50 +0530

    ✨ Wall height

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -114,26 +114,23 @@ const Walls = ({ length }) => {
       <RigidBody type="fixed" restitution={0.2} friction={0}>
         {/* Right Wall */}
         <mesh
-          position={[2.15, 0.75, -(length * 2) + 2]}
+          position={[2.15, 0.9, -(length * 2) + 2]} scale={[0.3, 2.2, 4 * length]}
           geometry={boxGeometry}
           material={wallMaterial}
-          scale={[0.3, 1.5, 4 * length]}
           castShadow
         />
         {/* Left Wall */}
         <mesh
-          position={[-1 * 2.15, 0.75, -(length * 2) + 2]}
+          position={[-1 * 2.15, 0.9, -(length * 2) + 2]} scale={[0.3, 2.2, 4 * length]}
           geometry={boxGeometry}
           material={wallMaterial}
-          scale={[0.3, 1.5, 4 * length]}
           receiveShadow
         />
         {/* End Wall */}
         <mesh
-          position={[0, 0.75, -(length * 4) + 2]}
+          position={[0, 0.9, -(length * 4) + 2]} scale={[4, 2.2, 0.3]}
           geometry={boxGeometry}
           material={wallMaterial}
-          scale={[4, 1.5, 0.3]}
           receiveShadow
         />
         <CuboidCollider
